8a7a1c65dcc28cc804d06ee0e5f9709967842e8b
sebamart12 <93790958+sebamart12@users.noreply.github.com>
2022-11-15 20:23:06 -0300

    DIP y ISP

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -3,6 +3,7 @@ namespace Full_GRASP_And_SOLID
 {
     public interface IPrinter
     {
-         void PrintRecipe(Recipe recipe);
+          //Modificado por DIP
+         void PrintRecipe(IRecipe recipe);
     }
 }
---
+++ src/Library/IRecipe.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Full_GRASP_And_SOLID
+{
+    public interface IRecipe
+    {
+
+    // Agregado por ISP
+    
+        string GetTextToPrint();
+
+    }
+}
